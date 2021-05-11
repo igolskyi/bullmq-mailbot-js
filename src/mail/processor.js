12ce0c79f372b4495c140a8467aef514ba814647
Iurii Golskyi <igolskyi@gmail.com>
2021-05-11 20:20:24 +0300

    mail.processor: refactoring

--- src/mail/processor.js
+++ src/mail/processor.js
@@ -4,33 +4,34 @@ const { smtp } = require('../config')
 
 const transporter = nodemailer.createTransport(smtp)
 
+const convertToPDF = async html => Promise.all(
+  html.map(async attachment => {
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
+
+    const page = await browser.newPage()
+    await page.setContent(attachment.html)
+
+    const pdf = await page.pdf({
+      format: 'a4',
+      printBackground: true,
+    })
+
+    await browser.close()
+
+    return {
+      filename: `${attachment.name}.pdf`,
+      content: pdf,
+    }
+  }),
+)
+
 module.exports = async job => {
   try {
     const attachments = job.data.htmlAttachments
-      ? await Promise.all(
-        job.data.htmlAttachments.map(async attachment => {
-          const browser = await puppeteer.launch({
-            headless: true,
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-          })
-
-          const page = await browser.newPage()
-
-          await page.setContent(attachment.html)
-
-          const pdf = await page.pdf({
-            format: 'a4',
-            printBackground: true,
-          })
-
-          await browser.close()
-
-          return {
-            filename: `${attachment.name}.pdf`,
-            content: pdf,
-          }
-        }),
-      )
+      ? await convertToPDF(job.data.htmlAttachments)
       : []
 
     const info = await transporter.sendMail({
@@ -46,5 +47,6 @@ module.exports = async job => {
     }
   } catch (e) {
     console.error(e)
+    return e
   }
 }
